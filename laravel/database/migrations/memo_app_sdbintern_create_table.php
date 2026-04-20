b52dc63899fa5533dc1ecf0487bcec01327e4dcd
satoshi <r202304703oz@jindai.jp>
2026-04-20 18:02:02 +0900

    マイグレーションファイル内の、->integer('created_at')から->timestamps();に変更した。そのあとmemosを更新した。

--- laravel/database/migrations/memo_app_sdbintern_create_table.php
+++ laravel/database/migrations/memo_app_sdbintern_create_table.php
@@ -13,7 +13,7 @@ return new class extends Migration {
         Schema::create('memos', function (Blueprint $table) {
             $table->id();
             $table->string('content');
-            $table->integer('created_at');
+            $table->timestamps();
             $table->boolean('deleted');
         });
     }
