3049a8b2c0717971c968c7f291651d4a1d9de364
satoshi <r202304703oz@jindai.jp>
2026-04-21 12:47:18 +0900

    Headerコンポーネントを作成

---
+++ laravel/resources/js/components/Header.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+
+import DocumentSvg  from "@/components/svgs/DocumentSvg.vue";
+
+</script>
+
+<template>
+    <header class="header">
+        <h1 class="title">
+            <DocumentSvg /><span class="span-orange">かんたんメモ</span></h1>
+        <h1>思いついたことをサッと記録しよう</h1>
+    </header>
+</template>
+
+<style>
+    .header {
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        padding: 20px 20px;
+    }
+    .title {
+        font-size: 40px;
+        font-weight: bold;
+        display: flex;
+        align-items: center;
+        gap: 8px;
+    }
+    .span-orange {
+        color: rgb(249, 115, 22);
+    }
+</style>
